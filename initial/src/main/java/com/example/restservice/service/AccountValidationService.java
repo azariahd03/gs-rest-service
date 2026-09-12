7589ef3f369d658b9db2af8d7cdf3fac31979e14
Azariah <azariahd0323@gmail.com>
2026-09-12 22:55:10 +0530

    BANK-104 Add global exception handling

--- initial/src/main/java/com/example/restservice/service/AccountValidationService.java
+++ initial/src/main/java/com/example/restservice/service/AccountValidationService.java
@@ -1,22 +1,26 @@
 package com.example.restservice.service;
 
 import com.example.restservice.dto.AccountValidationRequest;
+import com.example.restservice.exception.InvalidAccountException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountValidationService {
 
     public String validateAccount(AccountValidationRequest request){
-        if(request.getAccountNumber() == null || request.getAccountNumber().isEmpty()){
-            return "Invalid: Account number is required";
+
+        if (request.getAccountNumber() == null ||
+                request.getAccountNumber().isEmpty()) {
+
+            throw new InvalidAccountException("Account number is required");
         }
-        if (request.getIfscCode() == null ||
-                request.getIfscCode().isEmpty()) {
-            return "Invalid: IFSC code is required";
+
+        if (request.getIfscCode() == null || request.getIfscCode().isEmpty()) {
+
+            throw new InvalidAccountException("IFSC code is required");
         }
         return  "Account Details are valid";
 
     }
-
 
 }
